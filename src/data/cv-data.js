9909c40f339f243d3865cd0e3597aac73c410f36
luidev-ddtr <luistepetate1@gmail.com>
2025-11-15 14:16:07 -0600

    initial commit

---
+++ src/data/cv-data.js
@@ -0,0 +1,47 @@
+
+const cvData =
+{
+  "name": "Luis Tepetate",
+  "email": "luistepetate1@gmail.com",
+  "linkedin": "https://www.linkedin.com/in/luis-tepetate",
+  "education": {
+    "degree": "Software Engineer Bachelor's Degree",
+    "university": "Universidad Tecnológica del Valle del Mezquital (UTVM)",
+    "location": "Hidalgo, Mexico",
+    "dates": "Aug 2024 – Present"
+  }, 
+    "skills": {
+    "programming_languages": "Python, SQL, C#, JavaScript",
+    "web_development": "Flask, Django, FastAPI",
+    "frontend_basics": "React, Astro, Vite",
+    "version_control": "Git, GitHub",
+    "devops": "Automation, Deployment"
+    },
+  "experience": [
+  {
+    "company": "Ixmiquilpan Community Organization",
+    "role": "Full Stack Developer & Systems Consultant",
+    "location": "Ixmiquilpan, Hidalgo, Mexico",
+    "dates": "Jan 2025 – Oct 2025",
+    "responsibilities": [
+      "Designed and deployed a centralized Community Management Web System using Flask and SQLite, reducing record lookup time by 94% and improving decision-making efficiency across departments.",
+      "Automated attendance tracking and debt assessment workflows, increasing operational efficiency by over 80% and enabling real-time compliance validation.",
+      "Led the migration from physical ledgers to a persistent digital database with automated cloud backups, ensuring long-term data integrity and eliminating the risk of unauthorized deletions."
+    ]
+  }
+  ],
+  "awards": [
+    {
+        "title": "Praxis Hackathon",
+        "place": "6th Place",
+        "date": "March 2025",
+        "location": "Hidalgo, Mexico"
+    }
+  ],
+  "certifications": [
+    "Cisco: CCNA Routing and Switching Certification.",
+    "Cisco: CCNA Certified Network Associate"
+  ]
+};
+
+export default cvData;
---
+++ src/main.js
@@ -0,0 +1,79 @@
+import cvData from './data/cv-data.js';
+document.addEventListener('DOMContentLoaded', () => {
+  console.log(cvData);
+  renderCV(cvData);
+});
+
+
+function renderCV(data) {
+  const container = document.getElementById('cv-container');
+
+  const html = `
+    <header>
+      <h1>${data.name}</h1>
+      <div class="contact">
+        <a href="mailto:${data.email}"><i class="fas fa-envelope"></i> ${data.email}</a> |
+        <a href="${data.linkedin}" target="_blank"><i class="fab fa-linkedin"></i> ${data.linkedin}</a>
+      </div>
+    </header>
+
+    <section class="education">
+      <h2>EDUCATION</h2>
+      <div class="linea">-</div>
+      <div class="item">
+        <div class="education-dates"><strong>${data.education.dates}</strong><br>${data.education.location}</div>
+        <h3>${data.education.university}</h3>
+        <div class="details">${data.education.degree}</div>
+      </div>
+    </section>
+
+    <section class="skills">
+      <h2>SKILLS</h2>
+      <div class="linea">-</div>
+      <div class="skills-list">
+      <strong>Programming languages:</strong> ${data.skills.programming_languages}<br>
+      <strong>Web Development:</strong> ${data.skills.web_development}<br>
+      <strong>Version Control:</strong> ${data.skills.version_control}<br>
+      <strong>Devops:</strong> ${data.skills.devops}
+      </div>
+    </section>
+
+    <section class="experience">
+      <h2>EXPERIENCE</h2>
+      <div class="linea">-</div>
+      ${data.experience.map(job => `
+        <div class="job">
+          <h3>${job.company}</h3>
+          <div class="company">${job.role}</div><br>
+          <div class="dates"><strong>${job.dates}</strong><br>${job.location}</div>
+          <ul>
+            ${job.responsibilities.map(resp => `<li>${resp}</li>`).join('')}
+          </ul>
+        </div>
+      `).join('')}
+    </section>
+
+    <section class="awards">
+      <h2>AWARDS</h2>
+      <div class="linea">-</div>
+      ${data.awards.map(award => `
+        <div class="item">
+          <div class="award-entry-line">
+            <h3>${award.title} ${award.place}</h3>
+            <div class="details"><strong>${award.date}</strong>  - ${award.location}</div>
+          </div>
+        </div>
+      `).join('')}
+    </section>
+    <br>
+    <section class="certifications">
+      <h2>CERTIFICATIONS</h2>
+      <div class="linea">-</div>
+      <ul>
+        ${data.certifications.map(cert => `<li>${cert}</li>`).join('')}
+      </ul>
+    </section>
+  `;
+
+  container.innerHTML = html;
+}
